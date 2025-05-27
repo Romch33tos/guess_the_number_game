bef8c365596d7c33a695c96027b72ea1591939ec
Roman Khadraliev <Romch33tos@yandex.ru>
2025-05-27 22:16:44 +0700

    Добавлена возможность начать новую игру

--- guess_the_number.py
+++ guess_the_number.py
@@ -4,7 +4,10 @@ from tkinter import messagebox as mb
 import random
 
 count = 5
+
 randnum = random.randint(1, 10)
+
+#Списки с фразами
 
 list = ['Я загадал совсем другое число!\nПопробуй ещё разочек! :)', 'Нет-нет, мое число меньше!\nПробуй ещё!', 'А вот и не угадал!\nДам подсказку, моё число меньше!', 'Дай подумать...\nНет, моё число другое!', 'Эй, ну куда так много!\nЯ загадал число поменьше!', 'Другое! Вот только не скажу, какое)', 'Нет, я загадал другое число!\nДавай ещё раз!', 'Нетушки! Давай ещё раз!']
 
@@ -14,18 +17,48 @@ list3 = ['Ура! Ты угадал!', 'Молодец, ты угадал!', 'П
 
 list4 = ['Не расстраивайся!\nПовезет в другой раз!', 'Попытки кончились!\nТы проиграл! :(', '                                               GAME OVER!', 'Игра окончена!', 'Эх, попытки кончились!\nНичего, повезёт в другой раз!']
 
-def how_to_use(): 
-	answer = mb.showinfo(title = "Справка", message = "Инструкция", detail = "1. Введи число от 1 до 10.\n2. Нажми на кнопку «Играть».\n3. Используй кнопку «Выйти»,\nчтобы закрыть игру.")
+list5 = ['Я загадал новое число!\nУгадай, какое!', 'Погоди секундочку...\nВсё, я загадал новое число!', 'Попробуем ещё раз! Начинай!', 'Сыграем ещё раз! \nЯ загадал новое число!']
 
+list6 = ['Ещё!', 'Сейчас угадаю!', 'Вот сейчас точно угадаю!', 'Ещё разочек!', 'Ещё раз!', 'Угадал?', 'А это?', 'Я угадал?']
+
+#Инструкция
+
+def how_to_use(): 
+	answer = mb.showinfo(title = "Справка", message = "Инструкция", detail = "1. Введи число от 1 до 10.\n2. Нажми на кнопку «Играть».\n3. Если у тебя кончатся попытки, ты\nможешь начать заново, но уже с \nдругим числом ;-)\n4. Используй кнопку «Выйти»,\nчтобы закрыть игру.")
+
+#Правила игры
 def game_rules(): 
 	answer = mb.showinfo(title = "Справка", message = "Правила игры", detail = "1. Запрещены лишние символы,\nкроме цифр\n2. Нельзя вводить числа меньше\nнуля или больше десяти!")
 
+#Функция выхода
+
 def exit():
-        answer = mb.askyesno( title="Выход", message="Вы действительно хотите выйти?")
+        answer = mb.askyesno( title="Выход", message="Ты правда хочешь выйти?")
         if answer == True:
         	window.quit()
         else:
         	pass
+
+#Начать заново
+
+def restart():
+    answer = mb.askyesno(title = "Новая игра", message = "Ты действительно хочешь\n        начать заново? ")
+    if answer == True:
+    	   global count
+    	   count = 5    	   
+    	   attempts.configure(state = NORMAL)
+    	   attempts.delete("1.0", END)
+    	   attempts.insert("1.0", "     5")
+    	   attempts.configure(state = DISABLED)
+    	   text.configure(state = NORMAL)
+    	   text.delete("1.0", END)
+    	   text.insert("1.0", random.choice(list5))
+    	   text.configure(state = DISABLED)
+    	   num.configure(state = NORMAL)
+    	   num.delete(0, END)
+    	   R.configure(state = NORMAL, text = random.choice(list6))
+    
+#Игра
 
 def play():
     n = int(num.get())
@@ -38,7 +71,7 @@ def play():
         count = count - 1
       
     if n > randnum and n > 0 and n < 11:
-    	R.configure(text = "Ещё раз!")
+    	R.configure(text = random.choice(list6))
     	attempts.configure(state = NORMAL)
     	attempts.delete("1.0", END)
     	attempts.insert("1.0", "     " + str(count))
@@ -50,7 +83,7 @@ def play():
     	text.configure(state = DISABLED)
     	
     if n < randnum and n > 0 and n < 11:
-    	R.configure(text = "Ещё раз!")
+    	R.configure(text = random.choice(list6))
     	attempts.configure(state = NORMAL)
     	attempts.delete("1.0", END)
     	attempts.insert("1.0", "     " + str(count))
@@ -67,24 +100,30 @@ def play():
     	text.delete("1.0", END)
     	text.insert("1.0", random.choice(list3))
     	text.configure(state = DISABLED)
+    	num.configure(state = "readonly")
     	R.configure(state = DISABLED) 
 
-    if count == 0:
-    	R.configure(text = "Попытки кончились")
-    	num.delete(0, END)
-    	num.configure(state = "readonly")	
-    	text.configure(state = NORMAL)
-    	text.delete("1.0", END)
-    	text.insert("1.0", random.choice(list4))
-    	text.configure(state = DISABLED)
-    	R.configure(state = DISABLED) 	  	
+    if count == 0 and n != randnum:  	
+            R.configure(text = "Попытки кончились")
+            num.delete(0, END)
+            num.configure(state = "readonly")
+            text.configure(state = NORMAL)
+            text.delete("1.0", END)
+            text.insert("1.0", random.choice(list4))
+            text.configure(state = DISABLED)
+            R.configure(state = DISABLED) 	  	
+#Графический дизайн 
     	
 window = Tk()
+
+#Меню
 
 menubar = Menu(window)
 
 helpmenu = Menu(menubar, tearoff=0)
 menubar.add_cascade(label="Справка", menu=helpmenu)
+
+menubar.add_command(label = "Новая игра", command = restart)
 
 menubar.add_command(label = "Выйти", command = exit)
 
@@ -125,7 +164,7 @@ num.focus()
 
 #Кнопки
 
-R = Button(window, width = 16, text = "Играть", command = lambda: play())
-R.grid(row = 4, column = 0, sticky = NW, padx = 180, pady = 7)
+R = Button(window, width = 20, text = "Играть", command = lambda: play())
+R.grid(row = 4, column = 0, sticky = NW, padx = 150, pady = 7)
 
 window.mainloop()
